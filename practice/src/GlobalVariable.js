48e26bbcdddeb024e9f3fb308fd9f34559a13f4b
cyber0bravo <cyber0bravo@gmail.com>
2025-01-06 20:39:24 +0530

    Second Attempt

--- practice/src/GlobalVariable.js
+++ practice/src/GlobalVariable.js
@@ -1,3 +1,3 @@
-const CODING_API_URL = "https://urban-space-sniffle-q79gprwp655pc9qr6-5500.app.github.dev/api/";
-// const CODING_API_URL = "http://localhost:5000/api/";
+// const CODING_API_URL = "https://urban-space-sniffle-q79gprwp655pc9qr6-5500.app.github.dev/api/";
+const CODING_API_URL = "http://localhost:5000/api/";
 export {CODING_API_URL};
